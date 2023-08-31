9dd833bcb51fb5a611c1482cf3110e79278f73a5
DatsivTaras <datskn46@gmail.com>
2023-08-31 22:29:31 +0300

    fix

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -80,7 +80,9 @@
                         <div class="btn-group">
                             {{ 'Теги :' }}
                             @foreach($news->tags as $tag)
+                                <div class="btn-group me-2" role="group" aria-label="Second group">
                                 <a href='/search?query={{ $tag->name }}'>{{ $tag->name .' '}}  </a>
+                                </div>
                             @endforeach
                         </div>
                     </div>
