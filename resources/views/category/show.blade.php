0ee27943d173b0eade76f4cd5dfb887786735776
Hladyo.vasyl <developer11@stefanwarnat.de>
2023-09-14 14:36:29 +0300

    Fixed bugs with styles at news page

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -7,19 +7,20 @@
 @section('content')
     <div class="container">
         <div class="row home-content">
-            <div class="col-sm-3 main-widget-left mobile-hide">
-                <h3 class="main-widget-title">Стрічка новин</h3>
-                @widget('recentNews')
+        <h1 class="text-center news-category-title">
+            {{$category->getName()}}
+        </h1>
+            <div class="col-xl-3 col-lg-3 col-md-4 d-sm-none d-none d-md-block d-md-block">
+                <div class="main-widget-left">
+                    <h3 class="main-widget-title">Стрічка новин</h3>
+                    @widget('recentNews')
+                </div>
             </div>
-            <div class="col-sm-9">
-                <h1 class="text-center news-category-title">
-                    {{$category->getName()}}
-                </h1>
+            <div class="col-xl-9 col-lg-9 col-md-8 col-sm-12 col-12">
                 <div class="news row">
                     @include('news.parts._list-news')
                 </div>
                 {!! $news->links() !!}
-
             </div>
         </div>
     </div>
--- resources/views/news/parts/_list-news.blade.php
+++ resources/views/news/parts/_list-news.blade.php
@@ -1,15 +1,20 @@
 @foreach ($news as $new)
-    <div class="col-sm-4 three-news-blocks">
-        <a href="{{$new->getUrl()}}" style="text-decoration: none; color:black">
+    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12 three-news-blocks">
             <div class="card">
-                <div class="three-news-blocks-category"><div class="triangle">{{ $new->getcategoryName() }}</div></div>
+                <div class="three-news-blocks-category">
+                    <div class="triangle">
+                        <a class="category-link" href="#">{{ $new->getcategoryName() }}</a>
+                    </div>
+                </div>
                 <img class="card-img-top" src="{{ $new->getImageUrl() }}" alt="Card image cap">
                 <div class="card-body">
-                    <h5 class="card-title">{{ $new->title }}</h5>
+                    <a href="{{$new->getUrl()}}"><h5 class="card-title">{{ $new->title }}</h5></a>
 <!--                     <p class="card-text">{{ $new->mini_description }}</p> -->
                     <p class="source"><b>Джерело :</b> {{ $new->subtitle }}</p>
                     <div class="read-more-container">
-                        <p class="read-more">Далі > </p>
+                        <a href="{{$new->getUrl()}}">
+                            <p class="read-more">Далі > </p>
+                        </a>
                         <p class="date-news">{{ \Carbon\Carbon::parse($new->date_of_publication)->format('d.m.Y') }}</p>
                     </div>
                 </div>
