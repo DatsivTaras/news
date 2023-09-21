d55e5b4273885ef4a88f73e25cc9616d7e4a9df6
DatsivTaras <datskn46@gmail.com>
2023-09-21 14:58:14 +0300

    fix date for slider

--- resources/views/widgets/parts/_news.blade.php
+++ resources/views/widgets/parts/_news.blade.php
@@ -2,7 +2,7 @@
      <div class="{{$news->isImportment() ? 'main' : '' }} ">
          <p class="news-description">{{$news->getTitle()}}</p>
          @if(isset($type) && $type == 'popular')
-             <p class="news-time">{{ \Carbon\Carbon::parse($news->getPublicationDate())->format('d-m-Y h:i') }}</p>
+             <p class="news-time">{{ \Carbon\Carbon::parse($news->getPublicationDate())->format('h:i') }} {{ \Carbon\Carbon::parse($news->getPublicationDate())->format('d-m-Y ') }}</p>
          @else
              <p class="news-time">{{ \Carbon\Carbon::parse($news->getPublicationDate())->format('h:i') }}</p>
          @endif
